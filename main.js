892b67e6e66626e35b55e639305ae25c02eb268a
cnxontle <cnxontle@gmail.com>
2024-08-24 20:58:37 -0600

    se integra electon

---
+++ main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 1024,
+        height: 768,
+        webPreferences: {
+            nodeIntegration: false, 
+            contextIsolation: false,  
+            //preload: path.join(__dirname, 'preload.js')  
+        }
+    });
+
+    // Cargar la página de quantfury
+    mainWindow.loadURL('https://trading.quantfury.com/');
+
+    // Inyectar código JavaScript en la página
+    mainWindow.webContents.on('dom-ready', () => {
+        mainWindow.webContents.executeJavaScript(
+            require('fs').readFileSync(path.join(__dirname, 'quant.js'), 'utf8'));
+    });
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+          
+        }
+
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
