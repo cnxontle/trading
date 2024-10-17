df0b9a7b57a83100d1cefb69b2cbc10352c32987
cnxontle <cnxontle@gmail.com>
2024-10-17 12:25:38 -0600

    carga de soluciones

--- activador.js
+++ activador.js
@@ -14,12 +14,12 @@ const pool = new Pool({
 });
 
 const ws = new WebSocket('ws://localhost:55555');
-let isWsOpen = false;  
+let isWsOpen = false; 
+let soluciones; 
 
 // Funcion de Microclima
 function obtenerMclima(promedio) {
     let mclima;
-
     if (promedio > 2) {
         mclima = 'b';
     } else if (promedio > 0.5) {
@@ -28,8 +28,10 @@ function obtenerMclima(promedio) {
         mclima = 'o';
     } else if (promedio > -2) {
         mclima = 'q';
-    } else {
+    } else if (promedio <= -2) {
         mclima = 'p';
+    } else {
+        mclima = 'x';
     }
     return mclima;
 }
@@ -47,97 +49,96 @@ async function leerConsultaSQL() {
 
 // Función para ejecutar la consulta SQL
 async function ejecutarSQL(sql) {
-    try {
-        const results = await pool.query(sql);
-        if (results && results.rows.length === 2) {  // Aseguramos que haya exactamente 2 filas
-            const primeraFila = results.rows[0];
-            const segundaFila = results.rows[1];
-
-            // Obtenemos los valores de la primera y segunda columnas
-            const id = primeraFila.id;
-            const tiempo = primeraFila.tiempo;
-
-            // Creamos un nuevo objeto para almacenar los resultados de las operaciones desde la tercera columna en adelante
-            let resultadoFinal = {
-                id: id,
-                tiempo: tiempo
-            };
-
-            let sumaResultadosCripto = 0;
-            let totalColumnasCripto = 0;
-            let ResultadoSP = 0;
-            let ResultadoEner = 0;
-            let totalColumnasEner = 0;
-            let ResultadoComm = 0;
-            let totalColumnasComm = 0;
-
-            // Iteramos sobre todas las columnas (incluyendo más allá de la 32)
-            const columnas = Object.keys(primeraFila);
-            let resultados = {}; // Objeto para almacenar los resultados ya calculados
-            for (let i = 2; i < columnas.length; i++) {
-                const columna = columnas[i];
-                const valorPrimeraFila = primeraFila[columna];
-                const valorSegundaFila = segundaFila[columna];
-
-                // Realizamos la operación de pendiente
-                const resultado = ((valorPrimeraFila - valorSegundaFila) / valorSegundaFila) * 100;
-
-                // Almacenamos el resultado para reutilizar
-                resultadoFinal[columna] = resultado;
-                resultados[columna] = resultado;
-
-                // Solo acumulamos los resultados entre la columna 2 y 33
-                if (i <= 32) {
-                    sumaResultadosCripto += resultado;
-                    totalColumnasCripto++;
+    if (soluciones) {
+        try {
+            const results = await pool.query(sql);
+            if (results && results.rows.length === 2) {  // Se esperan 2 filas
+                
+                // Se accede a las soluciones y a las filas de resultados
+                const primeraSolucion = soluciones[0];
+                const segundaSolucion = soluciones[1];
+                const tercerSolucion = soluciones[2];
+                const cuartaSolucion = soluciones[3];
+                const quintaSolucion = soluciones[4];
+                const primeraFila = results.rows[0];
+                const segundaFila = results.rows[1];
+                const id = primeraFila.id;
+                const tiempo = primeraFila.tiempo;
+    
+                // se crea un objeto para almacenar los resultados
+                let resultadoFinal = {id: id, tiempo: tiempo};
+                let sumaResultadosCripto = 0;
+                let totalColumnasCripto = 0;
+                let ResultadoSP = 0;
+                let ResultadoEner = 0;
+                let totalColumnasEner = 0;
+                let ResultadoComm = 0;
+                let totalColumnasComm = 0;
+    
+                // Iteramos sobre todas las columnas (incluyendo más allá de la 32)
+                const columnas = Object.keys(primeraFila);
+                let resultados = {}; // Objeto para almacenar los resultados ya calculados
+                for (let i = 2; i < columnas.length; i++) {
+                    const columna = columnas[i];
+                    const valorPrimeraFila = primeraFila[columna];
+                    const valorSegundaFila = segundaFila[columna];
+                    const pendiente = ((valorPrimeraFila - valorSegundaFila) / valorSegundaFila) * 100;
+                    resultadoFinal[columna] = pendiente;
+    
+                    // Acumular sumas de pendientes
+                    if (i <= 32) {
+                        sumaResultadosCripto += pendiente;
+                        totalColumnasCripto++;
+                    }
+                    if (i == 74) {
+                        ResultadoSP = pendiente;
+                    }
+                    if (62 >= i <= 66){
+                        ResultadoEner += pendiente;
+                        totalColumnasEner++;
+                    }
+                    if (57 >= i <= 61){
+                        ResultadoComm += pendiente;
+                        totalColumnasComm++;
+                    }
+    
                 }
-                if (i == 74) {
-                    ResultadoSP = resultado;
-                }
-                if (62 >= i <= 66){
-                    ResultadoEner += resultado;
-                    totalColumnasEner++;
-                }
-                if (57 >= i <= 61){
-                    ResultadoComm += resultado;
-                    totalColumnasComm++;
-                }
-
+    
+                // Calcular el promedio de los resultados
+                const promedioCripto = sumaResultadosCripto / totalColumnasCripto;
+                const promedioEner = ResultadoEner / totalColumnasEner;
+                const promedioComm = ResultadoComm / totalColumnasComm;
+                const promedioSP = ResultadoSP;
+    
+                // Obtener microclimas
+                let mclimaCripto = obtenerMclima(promedioCripto);
+                let mclimaSP = obtenerMclima(promedioSP);
+                let mclimaEner = obtenerMclima(promedioEner);
+                let mclimaComm = obtenerMclima(promedioComm);
+                
+                // Añadir la columna 'clima' al resultado final
+                resultadoFinal.clima = mclimaCripto + mclimaSP + mclimaEner + mclimaComm;
+    
+                // Imprimimos el resultado final
+                console.log("clima:", resultadoFinal.clima, primeraSolucion);
+                
+                // Enviamos el resultado final al servidor WebSocket
+                //if (isWsOpen) {
+                //    ws.send(JSON.stringify(resultadoFinal));
+                //} else {
+                //    console.error('No se puede enviar el mensaje porque el WebSocket no está abierto.');
+                //}
+    
+            } else {
+                console.log("Se esperaban exactamente 2 filas, pero se obtuvieron:", results.rows.length);
             }
-
-            // Calculamos el promedio de los resultados de las columnas 2 a 32
-            const promedioCripto = sumaResultadosCripto / totalColumnasCripto;
-            const promedioEner = ResultadoEner / totalColumnasEner;
-            const promedioComm = ResultadoComm / totalColumnasComm;
-            const promedioSP = ResultadoSP;
-
-
-            // Asignamos la letra correspondiente en la columna 'clima' según el valor del promedio
-            let mclimaCripto = obtenerMclima(promedioCripto);
-            let mclimaSP = obtenerMclima(promedioSP);
-            let mclimaEner = obtenerMclima(promedioEner);
-            let mclimaComm = obtenerMclima(promedioComm);
-            
-            
-            // Añadimos la columna 'clima' al resultado final
-            resultadoFinal.clima = mclimaCripto + mclimaSP + mclimaEner + mclimaComm;
-
-            // Imprimimos el resultado final
-            console.log("Resultado final:", resultadoFinal);
-            
-            // Enviamos el resultado final al servidor WebSocket
-            //if (isWsOpen) {
-            //    ws.send(JSON.stringify(resultadoFinal));
-            //} else {
-            //    console.error('No se puede enviar el mensaje porque el WebSocket no está abierto.');
-            //}
-
-        } else {
-            console.log("Se esperaban exactamente 2 filas, pero se obtuvieron:", results.rows.length);
+        } catch (error) {
+            console.error("Error al ejecutar SQL:", error);
         }
-    } catch (error) {
-        console.error("Error al ejecutar SQL:", error);
+    } else {
+        console.log('Soluciones no está disponible aún');
     }
+    
 }
 
 // Función para escuchar notificaciones desde PostgreSQL
@@ -156,25 +157,34 @@ async function escucharNotificaciones() {
     }
 }
 
+// Función para cargar el archivo JSON
+async function cargarSoluciones() {
+    try {
+        const data = await fs.readFile(path.join(__dirname, 'soluciones.json'), 'utf8');
+        soluciones = JSON.parse(data);
+    } catch (error) {
+        console.error('Error al cargar el archivo JSON:', error);
+    }
+}
+
 // Manejar eventos del WebSocket
 ws.on('open', function open() {
     console.log('Conexión WebSocket establecida');
-    isWsOpen = true;  // Cambiar el estado de la conexión
+    isWsOpen = true; 
 });
-
 ws.on('error', function error(err) {
     console.error('Error en la conexión:', err);
 });
-
 ws.on('close', function close() {
     console.log('Conexión WebSocket cerrada');
-    isWsOpen = false;  // Cambiar el estado de la conexión
+    isWsOpen = false;
     pool.end(() => {
         console.log('Conexión a PostgreSQL cerrada');
     });
 });
 
 // Llamar a la función para escuchar notificaciones
+cargarSoluciones();
 escucharNotificaciones()
     .then(() => {
         console.log('Notificaciones activadas con éxito.');
