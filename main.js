6f64be1e580c61e11389c9f342eddc7321506373
cnxontle <cnxontle@gmail.com>
2024-11-14 16:57:27 -0600

    restauracion

--- main.js
+++ main.js
@@ -48,7 +48,13 @@ if (mode !== 1) {
     // Iniciar el script de PowerShell
     const sender = process.env.SO === 'windows' ? 'pwsh/windows.ps1' : 'pwsh/linux.ps1';
     const shell = spawn('pwsh', ['-File', sender]);
-   
+    let numericValue;
+
+    // Ejecutar promesa con tiempo de espera
+    async function withTimeout(promise) {
+        return Promise.race([promise, new Promise((_, reject) => setTimeout(() => reject(new Error("Timeout")), 10000))]);
+    }
+    
     // Función para enviar texto
     async function sendKeys(text) {
         return new Promise((resolve, reject) => {
@@ -76,151 +82,183 @@ if (mode !== 1) {
         });
 
     // Función para manejar los mensajes
-    const TIMEOUT = 10000;
-    const POLLING_INTERVAL = 100;
-
-    // Manejar mensajes recibidos
     async function handleMessage(ws, message) {  
         const data = JSON.parse(message);
         const activo = data.id;
         const operacion = data.operacion;
-        let stop_loss = calculateAdjustedPrice(data.stop_loss, operacion, -0.002);
-        let take_profit = calculateAdjustedPrice(data.take_profit, operacion, 0.002);
-        const boton_id = {comprar: 'buy', vender: 'sell'}[operacion];
-        
-        try {
-            if (data.accion === 'abrir') {
-                await handleOpenPosition(ws, activo, operacion, boton_id, take_profit, stop_loss);
-            } else if (data.accion === 'cerrar') {
-                await handleClosePosition(ws, activo);
-            }
-        } catch (error) { 
-            ws.send(JSON.stringify({status: 300}));
-            console.error('Error en operación:', error);
-        }
-    }
+        let stop_loss = parseFloat(data.stop_loss);
+        stop_loss += (operacion === 'comprar' ? -1 : 1) * stop_loss * 0.002;
+        let take_profit = parseFloat(data.take_profit);  
+        take_profit += (operacion === 'comprar' ? 1 : -1) * take_profit * 0.002;
+        const boton_id = {comprar: 'buy',vender: 'sell'}[operacion];
 
-    // Abrir posición
-    async function handleOpenPosition(ws, activo, operacion, boton_id, take_profit, stop_loss) {
-        console.log(`Intentando abrir una posición en ${activo} de ${operacion}`);
-        const balance = await getNumericValue('span[data-testid="default_margin_ballance"]');
-        if (balance < 10) {
-            console.log('Balance insuficiente');
-            return;
-        }
-        await withTimeout(openPosition(balance, activo, boton_id), TIMEOUT);
-        await withTimeout(defineOrder('takeProfit', balance, take_profit), TIMEOUT / 2);
-        await withTimeout(defineOrder('stopLoss', balance, stop_loss), TIMEOUT / 2);
-        ws.send(JSON.stringify({ status: 300 }));
-    }
+        // Código para intentar abrir una nueva posición..
+        if (data.accion === 'abrir') {
+            console.log(`Intentando abrir una posicion en ${activo} de ${operacion}`);
+            
+            try{
+                // Verificar la cantidad disponible en la cartera
+                numericValue = await mainWindow.webContents.executeJavaScript(`
+                    (() => {
+                        const span = document.querySelector('span[data-testid="default_margin_ballance"]');
+                        const rawValue = span ? span.textContent.trim() : '';
+                        const numericValue = rawValue.replace(/[^0-9.]/g, '');
+                        return numericValue;
+                    })();
+                `);
 
-    // Cerrar posición
-    async function handleClosePosition(ws, activo) {
-        console.log(`Intentando cerrar una posición en ${activo}`);
-        await mainWindow.webContents.executeJavaScript(`document.querySelector('button[data-testid="positions_tab"]').click();`);
-        await waitForSelectorAndExecute('button[data-testid="instrument_info_WHR"]', null, 50);
-        await mainWindow.webContents.executeJavaScript(`document.querySelector('button[data-testid="cross_${activo}"]').click();`);
-        const mercadoCerrado = !(await isConfirmButtonAvailable());
-
-        if (mercadoCerrado) {
-            const segundos = await getMarketClosedTime();
-            ws.send(JSON.stringify({ status: 400, segundos_restantes: segundos }));
-            return;
-        }
-
-        await mainWindow.webContents.executeJavaScript(`document.querySelector('button[data-testid="close_by_cross_modal_modal_confirm_button"]').click();`);
-        await waitForSelectorAndExecute(`//*[@id="root"]/div/div[1]/div`, async () => {
-            await mainWindow.webContents.executeJavaScript(`location.reload();`);
-            ws.send(JSON.stringify({ status: 401 }));
-        }, TIMEOUT);
-
-        ws.send(JSON.stringify({ status: 200 }));
-    }
-
-    // Función para obtener el balance en formato numérico
-    async function getNumericValue(selector) {
-        const value = await mainWindow.webContents.executeJavaScript(`
-            (() => {
-                const span = document.querySelector('${selector}');
-                return span ? parseFloat(span.textContent.replace(/[^0-9.]/g, '')) : 0;
-            })();
-        `);
-        return value * (apalancamiento / soluciones) || 0;
-    }
-
-    // Calcular precio ajustado
-    function calculateAdjustedPrice(price, operacion, adjustmentRate) {
-        let adjustedPrice = parseFloat(price);
-        adjustedPrice += (operacion === 'comprar' ? 1 : -1) * adjustedPrice * adjustmentRate;
-        return adjustedPrice;
-    }
-
-    // Función de espera y ejecución
-    async function waitForSelectorAndExecute(selector, action = null, maxIterations = Infinity) {
-        let iterations = 0;
-        while (await mainWindow.webContents.executeJavaScript(`document.querySelector('${selector}') === null`)) {
-            if (++iterations >= maxIterations) break;
-            await new Promise(resolve => setTimeout(resolve, POLLING_INTERVAL));
-        }
-        if (action) await mainWindow.webContents.executeJavaScript(action);
-    }
-
-    // Función para verificar si el botón de confirmación está disponible
-    async function isConfirmButtonAvailable() {
-        return await mainWindow.webContents.executeJavaScript(`
-            (() => {
-                const button = document.querySelector('button[data-testid="close_by_cross_modal_modal_confirm_button"]');
-                return button ? !button.disabled : false;
-            })();
-        `);
-    }
-
-    // Obtener tiempo restante si el mercado está cerrado
-    async function getMarketClosedTime() {
-        return await mainWindow.webContents.executeJavaScript(`
-            (() => {
-                const span = document.querySelector('span[data-testid="market_schedule_value"]');
-                if (span) {
-                    const match = span.textContent.match(/(\\d+)d\\s+(\\d+)h\\s+(\\d+)m/);
-                    if (match) {
-                        const [_, days, hours, minutes] = match.map(Number);
-                        return (days * 86400) + (hours * 3600) + (minutes * 60) + 60;
+                numericValue = parseFloat(numericValue);
+                numericValue *= (apalancamiento/soluciones); 
+                if (numericValue >= 10) {
+                    await mainWindow.webContents.executeJavaScript(`document.querySelector('button[data-testid="watchlist_tab"]').click();`);
+                    
+                    // Esperar a que se cargue la pestaña de watchlist
+                    while (await mainWindow.webContents.executeJavaScript(`document.querySelector('button[data-testid="instrument_info_WHR"]') === null`)) {
+                        await new Promise(resolve => setTimeout(resolve, 100));  
                     }
+
+                    // Abrir la ventana de información del activo e ingresar monto
+                    await mainWindow.webContents.executeJavaScript(`document.querySelector('button[data-testid="instrument_info_${activo}"]').click();`);
+                    await mainWindow.webContents.executeJavaScript(`document.querySelector('button[id="${boton_id}"]').click();`)
+                    await mainWindow.webContents.executeJavaScript(`document.querySelector('input[id="open_value_number_input"]').focus();`);
+                    await sendKeys(numericValue.toString());
+
+                    // Esperar a que se actualice el valor de input para abrir la posición
+                    while (await mainWindow.webContents.executeJavaScript(`document.querySelector('input[id="open_value_number_input"]').value === ''`)) {
+                        await new Promise(resolve => setTimeout(resolve, 100));
+                    }
+                    await mainWindow.webContents.executeJavaScript(`document.querySelector('button[id="open_position"]').click();`);
+                    
+                    // Esperar a que se termine de abrir la posicion
+                    while (await mainWindow.webContents.executeJavaScript(`document.querySelector('button[data-testid="instrument_info_WHR"]') !== null`)) {
+                        await new Promise(resolve => setTimeout(resolve, 100));
+                    }
+
+                    console.log('posicion abierta...');
+
+                    // Definir Orden Take Profit
+                    await mainWindow.webContents.executeJavaScript(`document.querySelector('span[id="target_order"]').click();`);
+                    await mainWindow.webContents.executeJavaScript(`document.querySelector('input[id="reduce_order_size_number_input"]').focus();`);
+                    await sendKeys(numericValue.toString());
+                    while (await mainWindow.webContents.executeJavaScript(`document.querySelector('input[id="reduce_order_size_number_input"]').value === ''`)) {
+                        await new Promise(resolve => setTimeout(resolve, 100));
+                    }
+                    await mainWindow.webContents.executeJavaScript(`document.querySelector('input[id="reduce_order_price_number_input"]').focus();`);
+                    await new Promise(resolve => setTimeout(resolve, 100));
+                    await sendKeys(take_profit.toString());
+                    while (await mainWindow.webContents.executeJavaScript(`document.querySelector('input[id="reduce_order_price_number_input"]').value === ''`)) {
+                        await new Promise(resolve => setTimeout(resolve, 100));
+                    }
+                    await mainWindow.webContents.executeJavaScript(`document.querySelector('button[data-testid="reduce_order_submit_button"]').click();`);
+                    await new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * 1000) + 2000));   // definir tiempo de espera
+                                      
+                    // Definir Orden Stop Loss
+                    await mainWindow.webContents.executeJavaScript(`document.querySelector('span[id="stop_order"]').click();`);
+                    await mainWindow.webContents.executeJavaScript(`document.querySelector('input[id="reduce_order_size_number_input"]').focus();`);
+                    await sendKeys(numericValue.toString());
+                    while (await mainWindow.webContents.executeJavaScript(`document.querySelector('input[id="reduce_order_size_number_input"]').value === ''`)) {
+                        await new Promise(resolve => setTimeout(resolve, 100));
+                    }
+                    await mainWindow.webContents.executeJavaScript(`document.querySelector('input[id="reduce_order_price_number_input"]').focus();`);
+                    await new Promise(resolve => setTimeout(resolve, 100));
+                    await sendKeys(stop_loss.toString());
+                    while (await mainWindow.webContents.executeJavaScript(`document.querySelector('input[id="reduce_order_price_number_input"]').value === ''`)) {
+                        await new Promise(resolve => setTimeout(resolve, 100));
+                    }
+                    await mainWindow.webContents.executeJavaScript(`document.querySelector('button[data-testid="reduce_order_submit_button"]').click();`);
+                    await new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * 1000) + 2000));   // definir tiempo de espera
+                    
+                    ws.send(JSON.stringify({status: 300}));
                 }
-                return null;
-            })();
-        `);
-    }
+            }catch (error) { 
+                ws.send(JSON.stringify({status: 300}));
+                console.error('error en apertura...'); }     
 
-    // Función para abrir posición
-    async function openPosition(numericValue, activo, boton_id) {
-        await mainWindow.webContents.executeJavaScript(`document.querySelector('button[data-testid="watchlist_tab"]').click();`);
-        await waitForSelectorAndExecute(`button[data-testid="instrument_info_${activo}"]`, `document.querySelector('button[data-testid="instrument_info_${activo}"]').click();`);
-        await waitForSelectorAndExecute(`button[id="${boton_id}"]`, `document.querySelector('button[id="${boton_id}"]').click();`);
-        await sendKeysToInput('input[id="open_value_number_input"]', numericValue);
-        await waitForSelectorAndExecute(`button[id="open_position"]`, `document.querySelector('button[id="open_position"]').click();`);
-        console.log('Posición abierta...');
-    }
+            // Código para intentar cerrar una posición
+        } else if (data.accion === 'cerrar') {      
+            console.log(`Intentando cerrar una posicion en ${activo}`);
+            let mercado_cerrado = false;
+            
+            try{
+                await mainWindow.webContents.executeJavaScript(`document.querySelector('button[data-testid="positions_tab"]').click();`);
+                
+                // Comprobar que nos encontramos en la pestaña de posiciones
+                let iterations = 0;
+                while (await mainWindow.webContents.executeJavaScript(`document.querySelector('button[data-testid="instrument_info_WHR"]') !== null`)) {
+                    if (iterations >= 50) {
+                        
+                        // Si no se encuentra la posición, enviar respuesta al nodo activador
+                        ws.send(JSON.stringify({status: 200}));
+                        return;  
+                    }
+                    await new Promise(resolve => setTimeout(resolve, 100));  
+                    iterations++;  
+                }
+                
+                // Click en el botón de cerrar posición
+                await mainWindow.webContents.executeJavaScript(`document.querySelector('button[data-testid="cross_${activo}"]').click();`);
 
-    // Definir orden de salida (Take Profit o Stop Loss)
-    async function defineOrder(type, numericValue, price) {
-        const orderSelector = type === 'takeProfit' ? 'target_order' : 'stop_order';
-        await waitForSelectorAndExecute(`span[id="${orderSelector}"]`, `document.querySelector('span[id="${orderSelector}"]').click();`);
-        await sendKeysToInput('input[id="reduce_order_size_number_input"]', numericValue);
-        await sendKeysToInput('input[id="reduce_order_price_number_input"]', price);
-        await waitForSelectorAndExecute(`button[data-testid="reduce_order_submit_button"]`, `document.querySelector('button[data-testid="reduce_order_submit_button"]').click();`);
-        await new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * 1000) + 2000));
-    }
+                // Verificar si el botón de confirmación está disponible
+                let confirmButtonAvailable = await mainWindow.webContents.executeJavaScript(`
+                    (() => {
+                        const confirmButton = document.querySelector('button[data-testid="close_by_cross_modal_modal_confirm_button"]');
+                        return confirmButton ? !confirmButton.disabled : false;
+                    })();
+                `);
+                
+                // Si el botón de confirmación no está disponible, el mercado está cerrado
+                if (!confirmButtonAvailable) {
+                    mercado_cerrado = true;
+                } else {
+                    await mainWindow.webContents.executeJavaScript(`document.querySelector('button[data-testid="close_by_cross_modal_modal_confirm_button"]').click();`);
+                }
+                
+                // Esperar a que la posición se cierre
+                let startTime = Date.now();
+                const max_time = 20000;
+                while (await mainWindow.webContents.executeJavaScript(`document.evaluate('//*[@id="root"]/div/div[1]/div', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue !== null`)) {
+                    
+                    // Si el modal no cierra recargar la página y enviar un mensaje 401
+                    if (Date.now() - startTime > max_time) {
+                        await mainWindow.webContents.executeJavaScript(`location.reload();`); 
+                        ws.send(JSON.stringify({status: 401}));
+                        return; 
+                    }
+                    await new Promise(resolve => setTimeout(resolve, 100));  
+                }
+                console.log('posicion cerrada...');
+            } catch (error) { console.error('error en cierre...'); }
 
-    // Ejecutar promesa con tiempo de espera
-    async function withTimeout(promise, timeout) {
-        return Promise.race([promise, new Promise((_, reject) => setTimeout(() => reject(new Error("Timeout")), timeout))]);
-    }
-
-    // Función para enviar teclas a un campo de entrada
-    async function sendKeysToInput(selector, value) {
-        await waitForSelectorAndExecute(selector, `document.querySelector('${selector}').focus();`);
-        await sendKeys(value.toString());
-        await waitForSelectorAndExecute(selector, `document.querySelector('${selector}').value !== ''`);
+            // Enviar respuesta al nodo activador
+            if (!mercado_cerrado) {             
+                ws.send(JSON.stringify({
+                    status: 200,
+                }));
+            }  else if (mercado_cerrado) {      
+                
+                // Cuando el mercado está cerrado, enviar el tiempo restante al nodo activador
+                let segundos = await mainWindow.webContents.executeJavaScript(`
+                (() => {
+                        const span = document.querySelector('span[data-testid="market_schedule_value"]');
+                        if (span) {
+                            const timeText = span.textContent;
+                            const timeMatch = timeText.match(/(\\d+)d\\s+(\\d+)h\\s+(\\d+)m/);
+                            if (timeMatch) {
+                                const days = parseInt(timeMatch[1], 10);
+                                const hours = parseInt(timeMatch[2], 10);
+                                const minutes = parseInt(timeMatch[3], 10);
+                                const totalSeconds = (days * 86400) + (hours * 3600) + (minutes * 60) + 60;
+                                return totalSeconds;
+                            }
+                        }
+                        return null;
+                    })();
+                `);               
+                ws.send(JSON.stringify({
+                status: 400,
+                segundos_restantes: segundos,         
+                }));
+            } 
+        }
     }
 }
